b92b4da0eafba692e789e83a3f778c0c5f030d17
Soleiu <himamegu847@gmail.com>
2020-12-07 20:54:24 +0900

    add_card_edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
   resources :list, only: %i(new create edit update destroy) do
-    resources :card, only: %i(new create show)
+    resources :card, only: %i(new create show edit update)
   end
 end
